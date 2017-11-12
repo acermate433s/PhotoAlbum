1bc557ae6e4c66eb5f38ef800908ebc1488fbcb3
Adrian Demavivas <adrian.demavivas@gmail.com>
2017-11-11 18:33:07 -0600

    Added IAlbumFactory interface

---
+++ PhotoAlbum.Core/IAlbumFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace PhotoAlbum.Core
+{
+    /// <summary>
+    /// Interface for factory of Album
+    /// </summary>
+    interface IAlbumFactory
+    {
+        /// <summary>
+        /// Creates an instance of an album.
+        /// </summary>
+        IAlbum Create();
+    }
+}
